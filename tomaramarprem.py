364fd9220deb38b9e6f6e2fb4f9916001b8cb659
thesudo284 <whitmanadon@gmail.com>
2025-03-06 19:57:01 +0530

    Add files via upload

---
+++ tomaramarprem.py
@@ -0,0 +1,22 @@
+x = "hello world"
+y = 12000
+a = True
+b = 12.5
+c = [1, 2, 3]
+d = (1, 2, 3)
+e = {1:"one", 2:"two", 3:"three"}
+
+print(x)
+print("type of x is", type(x))
+print(y)
+print("type of y is", type(y))
+print(a)
+print("type of y is", type(a))
+print(b)
+print("type of y is", type(b))
+print(c)
+print("type of y is", type(c))
+print(d)
+print("type of y is", type(d))
+print(e)
+print("type of y is", type(e))
